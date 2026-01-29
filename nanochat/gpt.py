497e82fad301d02f9a9be5ab351b192e55f429c4
Adrian <adrian.nenu@postgrad.manchester.ac.uk>
2026-01-29 14:53:48 +0000

    perf

--- nanochat/gpt.py
+++ nanochat/gpt.py
@@ -414,7 +414,6 @@ class GPT(nn.Module):
         softcap = 15 # smoothly cap the logits to the range [-softcap, softcap]
         logits = self.lm_head(x) # (B, T, padded_vocab_size) <- very big tensor, large amount of memory
         logits = logits[..., :self.config.vocab_size] # slice to remove padding
-        logits = logits.float() # switch to fp32 for logit softcap and loss computation
         logits = softcap * torch.tanh(logits / softcap) # squash the logits
 
         if targets is not None:
